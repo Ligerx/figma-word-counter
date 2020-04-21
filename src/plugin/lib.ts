5f8a38149c8c66a2d52f3d61e3974882d3c70235
Alex Wang <alex.wang@APPLYBITEFIFTY.workdayinternal.com>
2020-04-21 16:05:17 -0700

    port over logic for word counter

---
+++ src/plugin/lib.ts
@@ -0,0 +1,73 @@
+export function supportsChildren(
+  node: SceneNode
+): node is FrameNode | ComponentNode | InstanceNode | BooleanOperationNode {
+  return (
+    node.type === "FRAME" ||
+    node.type === "GROUP" ||
+    node.type === "COMPONENT" ||
+    node.type === "INSTANCE" ||
+    node.type === "BOOLEAN_OPERATION"
+  );
+}
+
+export type Counts = {
+  characters: number;
+  charactersNoSpaces: number;
+  words: number;
+};
+
+export function getCountsForNodes(nodes: SceneNode[]): Counts {
+  const textNodes = findAllTextNodesForNodes(nodes);
+  const strings = textNodesToStrings(textNodes);
+  return getCounts(strings);
+}
+
+function findAllTextNodesForNodes(nodes: SceneNode[]): TextNode[] {
+  return nodes.flatMap(findAllTextNodesForNode);
+}
+
+function findAllTextNodesForNode(node: SceneNode): TextNode[] {
+  if (supportsChildren(node) && node.children) {
+    return node.findAll(child => child.type === "TEXT") as TextNode[];
+  } else if (node.type === "TEXT") {
+    return [node];
+  }
+
+  return [];
+}
+
+function textNodesToStrings(nodes: TextNode[]): string[] {
+  return nodes.map(node => node.characters);
+}
+
+function getCounts(strings: string[]): Counts {
+  return {
+    characters: countCharacters(strings),
+    charactersNoSpaces: countCharactersNoSpaces(strings),
+    words: countWords(strings)
+  };
+}
+
+// counting words makes most sense with an array of strings instead of 1 concatenated string
+function countCharacters(strings: string[]): number {
+  let count = 0;
+  strings.forEach(string => (count += string.length));
+  return count;
+}
+
+function countCharactersNoSpaces(strings: string[]): number {
+  let count = 0;
+  strings.forEach(string => {
+    const stringNoSpaces = string.replace(/\s+/g, "");
+    count += stringNoSpaces.length;
+  });
+  return count;
+}
+
+function countWords(strings: string[]): number {
+  let count = 0;
+  strings.forEach(string => {
+    count += string.split(/\s+/g).length;
+  });
+  return count;
+}
